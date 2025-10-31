19d67aeeab3099fe9ed1a52d9711b28f6ee76b9d
Charles-de-Chabot <chabot0302@gmail.com>
2025-10-31 14:12:40 +0100

    Mise en place de l'intersection du formulaire

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("hello everybody");
+/*<div classe="error-message">Non valde! <!div>*/
+
+function handlerDomContentLoaded() {
+    console.log( "Dom Chargé");
+    //Element du DOM
+    const
+        elInputFirstname = document.getElementById("input-firstname"),
+        elInputLastname = document.getElementById("input-lastname"),
+        elInputEmail = document.getElementById("input-email");
+    
+    //Fonctions
+    //gestionnaires d'évenement
+    function handlerFormSubmit(evt){
+        //Bloque le comportement par defaut de cet événement
+        evt.preventDefault();
+
+        console.log("Formulaire soumis...");
+        
+    }
+
+    // INITIALISATON
+    //Ecouteur sur le "submit" du formulaire
+    document.forms[0].addEventListener('submit', handlerFormSubmit);
+}
+
+document.addEventListener('DOMContentLoaded', handlerDomContentLoaded);
